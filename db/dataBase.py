f5b5289b894f02a8caed323409cdf80ba23f7a0a
Juan Daniel <juanda554242@gmail.com>
2020-12-10 09:21:48 -0500

    registrar usuario funcionando

---
+++ db/dataBase.py
@@ -0,0 +1,65 @@
+from datetime import date
+from pydantic import BaseModel
+from typing import  Dict
+
+class UsuarioPN(BaseModel):
+    email: str
+    name: str
+    password: str
+
+class DocsPn(BaseModel):
+    id_docPN: int 
+    name_doc:str
+    frenov:date
+    fvenc: date
+    falarm: date
+    email: str
+
+UsuariosPn={
+    1: UsuarioPN(email="pepita@gmail.com",name="Pepita Perez",password="pepita01"),
+    2: UsuarioPN(email="juanito@gmail.com",name="Juan Trujillo",password="juanito2020")
+}
+
+DocumentosPn={
+    1: DocsPn(id_docPN=1,name_doc="SOAT",frenov=date(2021,1,22),fvenc=date(2021,1,23),falarm=date(2021,1,22),email="pepita@gmail.com"),
+    2: DocsPn(id_docPN=2,name_doc="Licencia de conducir",frenov='2022-11-30',fvenc='2022-11-30',falarm='2022-11-15',email="pepita@gmail.com"),
+    3: DocsPn(id_docPN=3,name_doc="poliza de medicina prepagada",frenov='2020-12-20',fvenc='2020-12-20',falarm='2020-12-12',email="juanito@gmail.com"),
+    4: DocsPn(id_docPN=4,name_doc="SOAT",frenov='2021-03-12',fvenc='2021-03-12',falarm='2021-03-12',email="juanito@gmail.com"),
+    5: DocsPn(id_docPN=5,name_doc="ASODOCS",frenov='2020-04-30',fvenc='2020-05-07',falarm='2020-04-25',email="juanito@gmail.com")  
+
+    
+}
+def lista_usuarios():
+    lista_usuarios=[]
+    for usuario in UsuariosPn:
+        lista_usuarios.append(UsuariosPn[usuario])
+    return lista_usuarios
+
+def registrar_usuario(usuarioPn: UsuarioPN):
+    if usuarioPn.email in UsuariosPn:
+        return False
+    else:
+        UsuariosPn[usuarioPn.email]=usuarioPn
+        return True
+
+def login_usuario(email):
+    if email in UsuariosPn:
+        return UsuariosPn[email]
+    else:
+        return False
+
+def obtener_Docs_email(email):
+    if email in DocumentosPn:
+        return DocumentosPn[email]
+    else:
+        return None
+
+def crear_doc(documentoPn:DocsPn):
+    if documentoPn.id_docPN in DocumentosPn:
+        return False
+    else:
+        DocumentosPn[documentoPn.id_docPN]=documentoPn
+        return True
+
+
+
--- main.py
+++ main.py
@@ -1,67 +1,29 @@
-from db.user_db import UserInDB
-from db.user_db import update_user, get_user
-from db.transaction_db import TransactionInDB
-from db.transaction_db import save_transaction
-from models.user_models import UserIn, UserOut
-from models.transaction_models import TransactionIn, TransactionOut
-import datetime
 from fastapi import FastAPI, HTTPException
-from fastapi.middleware.cors import CORSMiddleware
+import db.dataBase
 
+app = FastAPI()
 
-api = FastAPI()
+@app.get('/usuarios')   #prueba
+async def obtenerUsuarios():
+    return db.dataBase.lista_usuarios()
+    
 
-origins=["https://cajero-apt-jdvpl.herokuapp.com"]
+@app.post("/registrar-usuario")
+async def registrar(usuario:db.dataBase.UsuarioPN):
+    registro_exitoso=db.dataBase.registrar_usuario(usuario)
+    if registro_exitoso:
+        return {"msg":"Usuario Creado Correctamente"}
+    else:
+        raise HTTPException(status_code=400,detail="Este usuario ya existe en la base de datos")
 
-api.add_middleware(
-    CORSMiddleware,allow_origins=origins,
-    allow_credentials=True,allow_methods=["*"],allow_headers=["*"],
-    )
+@app.get('/documentos/{email}')   #prueba
+async def obtenerDocumentos(email):
+    return db.dataBase.obtener_Docs_email(email)
 
-@api.post("/user/auth/")
-async def auth_user(user_in: UserIn):
-
-    user_in_db = get_user(user_in.username)
-
-    if user_in_db == None:
-        raise HTTPException(status_code=404, detail="El usuario no existe")
-
-    if user_in_db.password != user_in.password:
-        return  {"Autenticado": False}
-
-    return  {"Autenticado": True}
-
-
-@api.get("/user/balance/{username}")
-async def get_balance(username: str):
-
-    user_in_db = get_user(username)
-
-    if user_in_db == None:
-        raise HTTPException(status_code=404, detail="El usuario no existe")
-
-    user_out = UserOut(**user_in_db.dict())
-
-    return  user_out
-
-
-@api.put("/user/transaction/")
-async def make_transaction(transaction_in: TransactionIn):
-
-    user_in_db = get_user(transaction_in.username)
-
-    if user_in_db == None:
-        raise HTTPException(status_code=404, detail="El usuario no existe")
-
-    if user_in_db.balance < transaction_in.value:
-        raise HTTPException(status_code=400, detail="No se tienen los fondos suficientes")
-
-    user_in_db.balance = user_in_db.balance - transaction_in.value
-    update_user(user_in_db)
-
-    transaction_in_db = TransactionInDB(**transaction_in.dict(), actual_balance = user_in_db.balance)
-    transaction_in_db = save_transaction(transaction_in_db)
-
-    transaction_out = TransactionOut(**transaction_in_db.dict())
-
-    return  transaction_out
+@app.post("/crear-documento")
+async def crear_doc(documento:db.dataBase.DocsPn):
+    documento_exitoso=db.dataBase.crear_doc(documento)
+    if documento_exitoso:
+        return {"msg":"Documento Creado Correctamente"}
+    else:
+        raise HTTPException(status_code=400,detail="Este documento ya existe en la base de datos")
